618470260e1001080c21473a1e62b777a40463c2
izharrr911 <muhammadidzhar2@gmail.com>
2025-03-13 14:29:53 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#243e36',  
+        secondary: '#7ca982',
+        tertiary: '#f1f7ed',
+        dark: '#0f172a',
+      },
+      container: {
+        center: true,
+        padding: '16px',
+      },
+      mode: "jit",
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
+
+
